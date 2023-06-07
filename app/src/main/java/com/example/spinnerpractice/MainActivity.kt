cdbf532bbdc1a4e9e34070056d53402a000a551b
Danil Skalyanskiy <dskalyanskkiy@gmail.com>
2023-06-07 21:21:49 +0300

    second commit

--- app/src/main/java/com/example/spinnerpractice/MainActivity.kt
+++ app/src/main/java/com/example/spinnerpractice/MainActivity.kt
@@ -1,19 +1,12 @@
 package com.example.spinnerpractice
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.View
 import android.widget.AdapterView
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.spinnerpractice.databinding.ActivityMainBinding
-import com.example.spinnerpractice.ui.theme.SpinnerPracticeTheme
+
 
 class MainActivity : ComponentActivity() {
 
@@ -28,6 +21,7 @@ class MainActivity : ComponentActivity() {
         loadcolorSpinner()
     }
 
+
     private fun loadcolorSpinner() {
         selectedColor = ColorList().defaultColor
         binding.colorSpinner.apply {
@@ -38,6 +32,7 @@ class MainActivity : ComponentActivity() {
                 override fun onItemSelected(p0: AdapterView<*>?, p1: View?, position: Int, p3: Long)
                 {
                     selectedColor = ColorList().basicColor()[position]
+                    binding.colorTextView.setTextColor(Color.parseColor(selectedColor.Name))
                 }
 
                 override fun onNothingSelected(p0: AdapterView<*>?){}
